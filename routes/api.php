f22c08571b79373a606998b3bf7d4c131e3ad6a8
Mattias <drachbar@gmail.com>
2024-05-28 00:15:05 +0200

    Hantera funktionalitet för inloggning och skapa konto

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ExampleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +11,6 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('/example', [ExampleController::class, 'index']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
